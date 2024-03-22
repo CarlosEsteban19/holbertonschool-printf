a5062c01e2504a6968ad69af9558d6860f827c22
Juan J Mendez Torres <jrjuanj01@gmail.com>
2024-03-22 12:08:51 -0400

    Added unsigned int function to struct in printf.c

--- printf.c
+++ printf.c
@@ -7,7 +7,7 @@
 int _printf(const char *format, ...)
 {
 	typeofprint array[] = {
-		{"c", print_c}, {"s", print_s}, {"d", print_num},
+		{"c", print_c}, {"s", print_s}, {"d", print_num}, {"u", print_u},
 		{"i", print_num}, {"r", print_r}, {"R", print_rot},
 		{NULL, NULL},
 	};
